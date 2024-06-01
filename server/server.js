25be2dec578847f86d0e530e46e165c161eeca9f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-01 07:32:14 +0530

    Update server.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- server/server.js
+++ server/server.js
@@ -4,3 +4,20 @@ require('dotenv').config();
 const cors = require('cors');
 const path = require('path'); 
 
+// app
+
+const app = express();
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+app.use(cors())
+app.use(express.json())
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server Running on PORT ${PORT}`)
+})
