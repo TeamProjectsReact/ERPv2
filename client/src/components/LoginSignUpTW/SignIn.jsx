78bbdae61f007257659be8bec25c5c7837615d23
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-03 14:22:47 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/LoginSignUpTW/SignIn.jsx
+++ client/src/components/LoginSignUpTW/SignIn.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BsBuildingFill } from "react-icons/bs";
+import { BsMortarboardFill } from "react-icons/bs";
 import { Link } from 'react-router-dom';
 
 const SignIn = () => {
@@ -24,9 +24,9 @@ const SignIn = () => {
                 <div className="bg-white py-16 px-8 rounded shadow-md w-full ">
                     <center className='text-gray-500'>
                         {/* change the Icon According to your needs */}
-                        <h1 className=''><BsBuildingFill className='h-20 w-auto'/></h1>
-                        <p className="pt-4 text-2xl">Welcome Back to</p>
-                        <p className="">ERP System</p>
+                        <h1 className=''><BsMortarboardFill className='h-20 w-auto'/></h1>
+                        <p className="pt-4 text-2xl">Welcome Back</p>
+                        <p className="">Your Project Name</p>
                     </center>
                     <hr className='my-2'/>
                     <div className="my-4">
--- client/src/components/LoginSignUpTW/SignUp.jsx
+++ client/src/components/LoginSignUpTW/SignUp.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BsBuildingFill } from "react-icons/bs";
+import { BsMortarboardFill } from "react-icons/bs";
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
@@ -25,9 +25,9 @@ const SignUp = () => {
                 <div className="bg-white py-16 px-8 rounded shadow-md w-full ">
                     <center className='text-gray-500'>
                         {/* change the Icon According to your needs */}
-                        <h1 className=''><BsBuildingFill className='h-20 w-auto'/></h1>
+                        <h1 className=''><BsMortarboardFill className='h-20 w-auto'/></h1>
                         <p className="pt-4 text-2xl">Welcome to</p>
-                        <p className="">ERP System at NIFS</p>
+                        <p className="">Your Project Name</p>
                     </center>
                     <hr className='my-2'/>
                     <div className="my-4">
