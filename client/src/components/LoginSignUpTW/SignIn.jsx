9c2f9007e87523a1c1fcedee992c67535534ff67
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-03 15:38:52 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/LoginSignUpTW/SignIn.jsx
+++ client/src/components/LoginSignUpTW/SignIn.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import { BsMortarboardFill } from "react-icons/bs";
 import { Link } from 'react-router-dom';
-
+import  NIFSLogo  from '../../assets/nifs_logo.png';
+ 
 const SignIn = () => {
     // for login data
     const [LoginData, SetLoginData] = useState({
@@ -24,9 +25,9 @@ const SignIn = () => {
                 <div className="bg-white py-16 px-8 rounded shadow-md w-full ">
                     <center className='text-gray-500'>
                         {/* change the Icon According to your needs */}
-                        <h1 className=''><BsMortarboardFill className='h-20 w-auto'/></h1>
+                        <h1 className=''><img src={NIFSLogo} alt="" /></h1>
                         <p className="pt-4 text-2xl">Welcome Back</p>
-                        <p className="">Your Project Name</p>
+                        <p className="">ERP System</p>
                     </center>
                     <hr className='my-2'/>
                     <div className="my-4">
