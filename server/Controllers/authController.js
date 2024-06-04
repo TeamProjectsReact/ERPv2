374feff093e439201d2dde853123e57db521107d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-04 17:56:29 +0530

    Update authController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -7,7 +7,21 @@ const authController = {
         const {username, email, password} = req.body;
 
         // check user is in database
-        const CheckUser = await User.findOne({  })
+        const CheckUser = await User.findOne({ email, username })
+
+        if(CheckUser) {
+            return res.json({ Error: "User Already in database"})
+        }
+        else{
+            const HashPass = await bcrypt.hash(password, 10);
+
+            const NewUser = new User({
+                username: username,
+                email: email,
+                password: HashPass,
+                Role: "User"
+            })
+        }
     }
 }
 
