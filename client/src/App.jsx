02f39913902bb88641c88bdd2370e37ad11f43d7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-05 07:04:56 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import SignIn from "./components/LoginSignUpTW/SignIn";
 import SignUp from "./components/LoginSignUpTW/SignUp";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import Dashboard from "./components/Dashboard/Dashboard";
+import DashHome from "./components/Dashboard/DashHome";
+
 
 export default function App() {
   return (
@@ -11,6 +13,9 @@ export default function App() {
         <Route path="/" element={<SignIn />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/Dashboard" element={<PrivateRoute ProtectRoute={<Dashboard /> }/> }/>
+        <Route path="/Dashboard/" element={<PrivateRoute ProtectRoute={<Dashboard /> }/> } >
+          <Route path="Home" element={<PrivateRoute ProtectRoute={<DashHome /> }/> }/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
