a0dcfc066035732380a4d1c8238af273fbfb5f76
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-05 07:24:33 +0530

    Update DashNav.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Dashboard/DashNav.jsx
+++ client/src/components/Dashboard/DashNav.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { BsPersonFill } from 'react-icons/bs'
 import { useNavigate } from 'react-router-dom'
 import  secureLocalStorage  from  "react-secure-storage"
 
@@ -6,15 +7,22 @@ import  secureLocalStorage  from  "react-secure-storage"
 const DashNav = () => {
     const navigate = useNavigate()
     //curent login user
-    const RoleUser = secureLocalStorage.getItem("Login1");
-    const EmailUser = secureLocalStorage.getItem("Login2");
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("Login2");
+
+    const [DropDown, SetDropDown] = useState(false)
     
     
     if(RoleUser !== null && EmailUser !== null){
         return (
             <div className='bg-white py-8 px-8 rounded-xl shadow-xl mb-8'>
-                <div className="flex ">
+                <div className="flex justify-between">
                     Dashboard
+
+                    <div className="flex text-gray-500">
+                        <p className=""><BsPersonFill className='text-2xl'/></p>
+                        <p className="pl-2 text-xl">{EmailUser}</p>
+                    </div>
                 </div>
             </div>
           )
