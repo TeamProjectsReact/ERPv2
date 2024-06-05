9beed38b4cd04dfd538d39881b601419b76bb01c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-05 06:39:18 +0530

    Update App.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/Dashboard" />
       </Routes>
     </BrowserRouter>
   )
