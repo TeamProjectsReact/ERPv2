8f52d5c39101a67d2be610eda1b51ad5d8460572
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-05 06:45:58 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignIn from "./components/LoginSignUpTW/SignIn";
 import SignUp from "./components/LoginSignUpTW/SignUp";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 export default function App() {
   return (
@@ -8,7 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/SignUp" element={<SignUp />} />
-        <Route path="/Dashboard" />
+        <Route path="/Dashboard" element={<PrivateRoute ProtectRoute={<Dashboard /> }/> }/>
       </Routes>
     </BrowserRouter>
   )
