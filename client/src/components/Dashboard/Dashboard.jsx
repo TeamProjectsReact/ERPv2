3e36af6f5363d02700c2ba3ee106b800f2415096
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-05 07:02:19 +0530

    Update Dashboard.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import DashSide from './DashSide';
+import { Outlet } from 'react-router-dom';
+import DashNav from './DashNav';
+import DashSideTest from './DashSideTest';
+
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className="flex">
+      <DashSide />
+      {/* <DashSideTest /> */}
+      <div className="flex-1 px-2 py-4 bg-gray-200">
+        <DashNav />
+        <Outlet />
+      </div>
+    </div>
   )
 }
 
