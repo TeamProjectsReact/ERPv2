ac76777d03499ff944cff1e24ef398a311913ac3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-05 07:20:56 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Dashboard/DashNav.jsx
+++ client/src/components/Dashboard/DashNav.jsx
@@ -7,28 +7,14 @@ const DashNav = () => {
     const navigate = useNavigate()
     //curent login user
     const RoleUser = secureLocalStorage.getItem("Login1");
-    const EmailUser = secureLocalStorage.getItem("login2");
-    const [DarKData, SetDataDark] = useState()
-
-    useEffect(() => {
-        const DarkMode = localStorage.getItem('darkMode');
-        SetDataDark(DarkMode)
-    }, [])
+    const EmailUser = secureLocalStorage.getItem("Login2");
     
     
     if(RoleUser !== null && EmailUser !== null){
         return (
             <div className='bg-white py-8 px-8 rounded-xl shadow-xl mb-8'>
                 <div className="flex ">
-                    {
-                        (() => {
-                            if(RoleUser === "SuperAdmin"){
-                                return (
-                                    <p className="text-gray-500 text-2xl font-semibold">SuperAdmin Dashbord</p>
-                                )
-                            }
-                        })()
-                    }
+                    Dashboard
                 </div>
             </div>
           )
--- client/src/components/Dashboard/DashSide.jsx
+++ client/src/components/Dashboard/DashSide.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import  secureLocalStorage  from  "react-secure-storage"
 
-import { BsBackpack2Fill, BsSpeedometer2, BsPersonVideo3, BsFileEarmarkText, BsPeople, BsPersonGear, BsPower, BsBook, BsPatchCheck, BsBuilding, BsFile, BsMortarboard, BsList, BsX, BsCalendarEvent, BsBell, BsFilesAlt, BsFileText, BsCashCoin } from "react-icons/bs";
+import { BsBackpack2Fill, BsSpeedometer2, BsPersonVideo3, BsFileEarmarkText, BsPeople, BsPersonGear, BsPower, BsBook, BsPatchCheck, BsBuilding, BsFile, BsMortarboard, BsList, BsX, BsCalendarEvent, BsBell, BsFilesAlt, BsFileText, BsCashCoin, BsMortarboardFill } from "react-icons/bs";
 
 
 
@@ -10,8 +10,7 @@ const DashSide = () => {
     const navigate = useNavigate()
     //curent login user
     const RoleUser = secureLocalStorage.getItem("Login1");
-    const EmailUser = secureLocalStorage.getItem("login2");
-    const DarkMode = localStorage.getItem('darkMode');
+    const EmailUser = secureLocalStorage.getItem("Login2");
 
     // alert(DarkMode)
 
@@ -35,19 +34,7 @@ const DashSide = () => {
         window.location.reload()
     }
     
-    const [ModeDark, SetModeDark] = useState(false)
-
-    // for darkmode
-    const healeDarkMode = () => {
-        SetModeDark(true)
-    }
-
-    const healeLightkMode = () => {
-        SetModeDark(false)
-    }
-
         
-    localStorage.setItem("darkMode", ModeDark) 
     return (
         <div className="min-h-screen">
             <button
@@ -60,27 +47,14 @@ const DashSide = () => {
             </button>
             <div className={`lg:min-h-screen lg:pr-16 md:relative fixed overflow-auto top-0 left-0 h-full px-4 bg-gray-800 text-white w-auto transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0 transition-transform duration-300 ease-in-out`}>
                 <div className="flex whitespace-nowrap lg:py-8">
-                    <p className="text-2xl my-4 "><BsMortarboard /></p>                    
-
-                            <div className="my-4">
-                                <h1 className="lg:text-2xl text-xl my-2 pl-2">ABC Campus</h1>
-                                {
-                                    (() => {
-                                        if(RoleUser === "TO"){
-                                            return (
-                                                <p className='text-sm'>TO</p>
-                                            )
-                                        }
-
-                                    })()
-                                }
-                                
-                            </div>
-
-
-                    
+                    <p className="text-2xl my-4 pt-1"><BsMortarboardFill className='text-4xl'/></p>     
+                        <div className="my-4">
+                            <h1 className="lg:text-2xl text-xl my-2 pl-4">ERP System</h1>
+                            <p className="">SuperAdmin</p>  
+                        </div>                   
+                   
                 </div>
-                <hr className={` ${DarkMode ? 'border-gray-400' : 'border-white'}`}/>
+                <hr className={`border-white`}/>
                 
                 <div className="my-8">
 
