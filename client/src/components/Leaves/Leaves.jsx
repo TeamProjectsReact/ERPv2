ca450c1cc24b001e534ff895426af22014f6b3b9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-06 11:24:40 +0530

    Update Leaves.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Leaves/Leaves.jsx
+++ client/src/components/Leaves/Leaves.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Leaves = () => {
   return (
-    <div className='ml-4'>Leaves</div>
+    <div className='ml-4'>
+      <div className="">
+        <h1 className="text-xl font-semiblod text-gray-500">Leaves</h1>
+        <div className="my-4">
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
