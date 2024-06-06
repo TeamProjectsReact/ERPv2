d6eee3207a8d1df89d50ba63083f3a9606f3353e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-06 09:59:44 +0530

    Update DashFooter.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Dashboard/DashFooter.jsx
+++ client/src/components/Dashboard/DashFooter.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const DashFooter = () => {
   return (
-    <div className='bg-gray-500'>DashFooter</div>
+    <div className='bg-gray-500 text-white py-8 px-4 rounded my-4 text-center font-semibold'>
+        &copy; The ERP System v2 at NIFS || Designed and Developed By NIFS
+    </div>
   )
 }
 
