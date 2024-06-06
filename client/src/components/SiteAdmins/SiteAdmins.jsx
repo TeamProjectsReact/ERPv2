0b5a7f6ab2fffacc64b85431b0562dbc44831281
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-06 15:55:52 +0530

    Update SiteAdmins.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/SiteAdmins/SiteAdmins.jsx
+++ client/src/components/SiteAdmins/SiteAdmins.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BsBoxArrowDownRight } from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage";
+import CountUp from 'react-countup';
+
 
 const SiteAdmins = () => {
-  return (
-    <div>SiteAdmins</div>
-  )
+    
+    const navigate = useNavigate()
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("Login2");
+
+    if(EmailUser !== null && RoleUser !== null && RoleUser === "SuperAdmin"){
+        return (
+            <div>SiteAdmins</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
 }
 
 export default SiteAdmins
