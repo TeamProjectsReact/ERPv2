31327aa122ddb8177f7af4eac71d0b73c2b0302a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-06 11:34:55 +0530

    Update Leaves.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Leaves/Leaves.jsx
+++ client/src/components/Leaves/Leaves.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
 
 const Leaves = () => {
-  return (
-    <div className='ml-4'>
-      <div className="">
-        <h1 className="text-xl font-semiblod text-gray-500">Leaves</h1>
-        <div className="my-4">
-          
+  const navigate = useNavigate()
+  //curent login user
+  const EmailUser = secureLocalStorage.getItem("Login1");
+  const RoleUser = secureLocalStorage.getItem("Login2");
+
+  if(EmailUser !== null && RoleUser !== null){
+    return (
+      <div className='ml-4'>
+        <div className="">
+          <h1 className="text-xl font-semiblod text-gray-500">Leaves</h1>
+          <div className="my-4">
+            hi all
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
+  else{
+    useEffect(() => {
+      localStorage.clear()
+      navigate('/')
+    }, [])
+  }
 }
 
 export default Leaves
