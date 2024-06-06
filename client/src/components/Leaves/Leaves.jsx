9b74d4524d0f8dc9514c68f2f8bcd09d7c82a259
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-06 11:49:50 +0530

    Update Leaves.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Leaves/Leaves.jsx
+++ client/src/components/Leaves/Leaves.jsx
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from 'react'
+import { BsBoxArrowDownRight } from 'react-icons/bs'
 import { useNavigate } from 'react-router-dom'
-import  secureLocalStorage  from  "react-secure-storage"
+import  secureLocalStorage  from  "react-secure-storage";
+import CountUp from 'react-countup';
 
 const Leaves = () => {
+  const LeaveData = [
+    {id: 1, name: "Leaves", icon: <BsBoxArrowDownRight />, value: <CountUp end={20} />, style: "text-green-500"},
+    {id: 2, name: "Leaves", icon: <BsBoxArrowDownRight />, value: <CountUp end={20} />, style: "text-green-500"},
+    {id: 3, name: "Leaves", icon: <BsBoxArrowDownRight />, value: <CountUp end={20} />, style: "text-green-500"},    
+  ]
+
   const navigate = useNavigate()
   //curent login user
   const EmailUser = secureLocalStorage.getItem("Login1");
@@ -14,7 +22,30 @@ const Leaves = () => {
         <div className="">
           <h1 className="text-xl font-semiblod text-gray-500">Leaves</h1>
           <div className="my-4">
-            hi all
+          <div className="">
+                <div className="md:grid grid-cols-4 gap-4 mr-4">
+                  {
+                    LeaveData.map((data) => {
+                      return(
+                        <div className={`bg-white w-full mx-2 md:my-0 my-2 duration-500 rounded shadow-md ${data.style}`}>
+                          <div className="flex py-6 px-8 w-full justify-between border border-gray-200 rounded">
+                              <div className="">
+                                  <h1 className={`text-[180%] ${data.style}`}>{data.value}</h1>
+                                  <p className="py-2 text-[120%]">{data.name}</p>
+                              </div>
+                              <div className="">
+                                  <p className="text-[200%] text-gray-500">{data.icon}</p>
+                              </div>
+                          </div>
+                          <div className="text-white py-4 px-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded">
+                              All {data.name} 
+                          </div>
+                        </div>
+                      )
+                    })
+                  }                
+                </div>
+              </div>
           </div>
         </div>
       </div>
