db38fa9b37527cccccfd769b2450856dd0ee3448
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-09 20:12:50 +0530

    Update LeaveController.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/LeaveController.js
+++ server/Controllers/LeaveController.js
@@ -1,12 +1,12 @@
 const User = require('../Models/User')
 
 const LeaveController = {
-    HodData: async (res, req) => {
+    HodData: async (req, res) => {
         try{
             const hodData = await User.find({ Role: "hod" })
 
             if(hodData){
-                return res.json({ Result: hodData})
+                return res.json({ Result: hodData })
             }
         }
         catch (err) {
