8c5a91025aaa056bd2fad13714097151151fcdcb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-09 20:30:14 +0530

    Update AddLeaves.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Leaves/AddLeaves.jsx
+++ client/src/components/Leaves/AddLeaves.jsx
@@ -19,20 +19,22 @@ const AddLeaves = () => {
     .catch(err => console.log(err)) 
   }, [])
 
+  // send leave data to backend
+  const [LeaveData, SetLeaveData] = useState({
+    startTime: '',
+    hodEmail: '',
+    startDate: '',
+    endDate: '',
+    Dutarion: ''
+  })
+
+  console.log(LeaveData)
+
 
   if(RoleUser !== null && EmailUser !== null){
     return (
       <div>
         <div className="bg-white py-4 px-8 rounded shadow-md">
-          {
-            hodD.map((hod) => {
-              return (
-                <div className="" key={hod._id}>
-                  {hod.email} asdasd
-                </div>
-              )
-            })
-          }
           <h1 className="font-semibold text-gray-500 text-2xl">New Leave</h1>
           <hr />
           <div className="my-4">
@@ -41,42 +43,46 @@ const AddLeaves = () => {
                 <div className="my-4">
                   <label htmlFor="" className="text-gray-500">Start Time</label>
                   <input type="time" name="" id="" className="text-gray-700 h-12 w-full my-2 rounded bg-gray-200 shadow-md pl-2" required placeholder='Enter Start Time' 
-                  />
+                  onChange={e => SetLeaveData({...LeaveData, startTime:e.target.value})}/>
                 </div>
 
                 <div className="my-4">
                   <label htmlFor="" className="text-gray-500">Email</label>
                   <input type="email" name="" id="" className="text-gray-700 h-12 w-full my-2 rounded bg-gray-200 shadow-md pl-2" required disabled placeholder='Enter Start Time' 
-                  value={EmailUser}/>
+                  value={EmailUser} />
                 </div>
 
                 <div className="my-4">
                   <label htmlFor="" className="text-gray-500">HOD Email</label>
-                  <select name="" id="" className='text-gray-700 h-12 w-full my-2 rounded bg-gray-200 shadow-md pl-2'>
+                  <select name="" id="" className='text-gray-700 h-12 w-full my-2 rounded bg-gray-200 shadow-md pl-2'
+                   onChange={e => SetLeaveData({...LeaveData, hodEmail:e.target.value})}>
                     <option value="">Select One</option>
-                    <option value="hod2@123.com">hod1@123.com</option>
-                    <option value="hod2@123.com">hod2@123.com</option>
-                    <option value="hod2@123.com">hod3@123.com</option>
-                    <option value="hod2@123.com">hod4@123.com</option>
+                    {
+                      hodD.map((hod) => {
+                        return (
+                          <option value={hod.email}>{hod.email} - {hod.username}</option>
+                        )
+                      })
+                    }
                   </select>
                 </div>
 
                 <div className="my-4">
                   <label htmlFor="" className="text-gray-500">Start Date</label>
                   <input type="date" name="" id="" className="text-gray-700 h-12 w-full my-2 rounded bg-gray-200 shadow-md pl-2" required placeholder='Enter Start Date' 
-                  />
+                   onChange={e => SetLeaveData({...LeaveData, startDate:e.target.value})}/>
                 </div>
 
                 <div className="my-4">
                   <label htmlFor="" className="text-gray-500">End Date</label>
                   <input type="date" name="" id="" className="text-gray-700 h-12 w-full my-2 rounded bg-gray-200 shadow-md pl-2" required placeholder='Enter End Date' 
-                  />
+                  onChange={e => SetLeaveData({...LeaveData, endDate:e.target.value})}/>
                 </div>
 
                 <div className="my-4">
                   <label htmlFor="" className="text-gray-500">Dutarion</label>
                   <input type="text" name="" id="" className="text-gray-700 h-12 w-full my-2 rounded bg-gray-200 shadow-md pl-2" required placeholder='Enter Dutarion' 
-                  />
+                  onChange={e => SetLeaveData({...LeaveData, Dutarion:e.target.value})}/>
                 </div>                
               </div>
 
