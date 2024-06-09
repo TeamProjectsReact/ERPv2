aa5f65e02d4f26e46379ccb63fd2e05feae00027
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-09 19:58:50 +0530

    Update AddLeaves.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Leaves/AddLeaves.jsx
+++ client/src/components/Leaves/AddLeaves.jsx
@@ -2,13 +2,22 @@ import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import  secureLocalStorage  from  "react-secure-storage"
 import CountUp from 'react-countup'
-
+import axios from 'axios'
 
 const AddLeaves = () => {
   const navigate = useNavigate()
   //curent login user
   const EmailUser = secureLocalStorage.getItem("Login1");
   const RoleUser = secureLocalStorage.getItem("Login2");
+
+  // get all hods in database
+  const [hodData, SetHodData] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/leave/hodData')
+    .then(res => SetHodData(res.data.Result))
+    .catch(err => console.log(err)) 
+  }, [])
 
   if(RoleUser !== null && EmailUser !== null){
     return (
