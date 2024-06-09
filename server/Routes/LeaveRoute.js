f8c789ada404a3140626a81094b248af560e993f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-09 20:36:04 +0530

    Update LeaveRoute.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Routes/LeaveRoute.js
+++ server/Routes/LeaveRoute.js
@@ -4,6 +4,6 @@ const LeaveController = require('../Controllers/LeaveController')
 const router = express.Router()
 
 router.get('/hodData', LeaveController.HodData);
-
+router.post('/AddLeave', LeaveController.AddLeave);
 
 module.exports = router;
