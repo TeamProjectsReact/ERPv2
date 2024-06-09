4059c096c6462b8d8d4165fd2426bfa335073662
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-09 20:43:56 +0530

    Update Leave.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ server/Models/Leave.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const LeaveSchema = new mongoose.Schema({
+    StartTime: {
+        type: String,
+        required: true,
+    },
+    reqEmail: {
+        type: String,
+        required: true,
+    },
+    hodEmail: {
+        type: String,
+        required: true,
+    },
+    StartData: {
+        type: Date,
+        required: true,
+    },
+    EndDate: {
+        type: Date,
+        required: true,
+    },
+    Dutarion: {
+        type: String,
+        required: true,
+    }
+})
+
+const Leave = mongoose.model('Leave', LeaveSchema)
+
+module.exports = Leave;
