718c456285e72d8453631fd8b60551e8672ca2e9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-10 20:29:51 +0530

    Update AdminDeptadd.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Employees/AdminDeptadd.jsx
+++ client/src/components/Employees/AdminDeptadd.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from 'react-countup'
+import axios from 'axios'
 
 // when superadmin director or secetry add employee to department 
 
 const AdminDeptadd = () => {
-  return (
-    <div>AdminDeptadd</div>
-  )
+    const navigate = useNavigate()
+    //curent login user
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("Login2");
+
+    if(RoleUser === "SuperAdmin" || RoleUser === "Director" || RoleUser === "Secretary" ){
+        return (
+            <div>AdminDeptadd</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
 }
 
 export default AdminDeptadd
