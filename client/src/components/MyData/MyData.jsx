85a71e0af214addc977504c2962edcd1f0663f0b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-10 16:17:15 +0530

    Update MyData.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/MyData/MyData.jsx
+++ client/src/components/MyData/MyData.jsx
@@ -17,8 +17,8 @@ const MyData = () => {
           try {
             const response = await axios.get('https://teamprojectsreact.github.io/EMPAPI/EmpRestAPI.employees.json');
             const foundEmployee = response.data.find(emp => emp.email === EmailUser);
-            console.log(foundEmployee)
-            // SetUserData(foundEmployee);
+            // console.log(foundEmployee)
+            SetUserData(foundEmployee);
           } catch (error) {
             console.log(err)
           } 
@@ -29,11 +29,92 @@ const MyData = () => {
 
     if(EmailUser !== null && RoleUser !== null){
         return (
-            <div className='mx-8 my-4'>
+            <div className='md:mx-8 mx-2 my-4'>
                 <h1 className="text-gray-500 font-semibold text-xl">My Profile</h1>
 
-                <div className="bg-white py-4 px-8 my-4 mx-2 rounded shadow-md">
-
+                <div className="bg-white py-4 md:px-8 px-4 my-4 mx-2 rounded shadow-md">
+                    <table border={0}>
+                        <tr>
+                            <td>
+                                <p className="text-gray-500">Employee ID</p>
+                            </td>
+                            <td>
+                                {UserData.EmpID}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p className="text-gray-500">Initial</p>
+                            </td>
+                            <td>
+                                {UserData.initial} {UserData.surname}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p className="text-gray-500">Address</p>
+                            </td>
+                            <td>
+                                {UserData.address} 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p className="text-gray-500">Phone</p>
+                            </td>
+                            <td>
+                                {UserData.phone} 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p className="text-gray-500">Email</p>
+                            </td>
+                            <td>
+                                {UserData.email} 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p className="text-gray-500">Designation</p>
+                            </td>
+                            <td>
+                                {UserData.designation} 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p className="text-gray-500">NIC</p>
+                            </td>
+                            <td>
+                                {UserData.nic} 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p className="text-gray-500">Contact</p>
+                            </td>
+                            <td>
+                                {UserData.emgcontact}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p className="text-gray-500">Employee type</p>
+                            </td>
+                            <td>
+                                {UserData.type}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p className="text-gray-500">Civil Status</p>
+                            </td>
+                            <td>
+                                {UserData.civilstatus}
+                            </td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         )
