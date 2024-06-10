0f86db1282c93039da4012e98c8a096090505c95
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-10 20:18:38 +0530

    Create DeptRoute.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

---
+++ server/Routes/DeptRoute.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const DeptController = require('../Controllers/DeptController')
+
+const router = express.Router()
+
+router.get('/UsertoDept', DeptController.SeeUsers)
+
+module.export = router;
