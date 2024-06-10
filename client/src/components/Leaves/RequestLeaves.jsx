3cedf337a80850f57016536b924a649b8f1a0e2b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-10 11:05:46 +0530

    Update RequestLeaves.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Leaves/RequestLeaves.jsx
+++ client/src/components/Leaves/RequestLeaves.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const RequestLeaves = () => {
   return (
-    <div>RequestLeaves</div>
+    <div className='mx-4 my-8'>
+        <h1 className="text-gray-500 text-xl font-semibold">Requested Leaves</h1>
+    </div>
   )
 }
 
