dc9ec9938b0687de70870261606a270b6713c9fb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-10 16:39:20 +0530

    Update Employee.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Employees/Employee.jsx
+++ client/src/components/Employees/Employee.jsx
@@ -13,10 +13,38 @@ const Employee = () => {
     if(RoleUser === "SuperAdmin" || RoleUser === "Director" || RoleUser === "Secretary" || RoleUser === "HOD"){
         return (
             <div className='mx-4'>
-                <div className="bg-white py-4 px-6 rounded shadow-md">
-                    
+                    <div class="py-2 px-4 rounded shadow-md overflow-x-auto my-8 bg-white">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <th scope="col" class=" table-cell">
+                                    <p className="md:block hidden">Email</p>
+                                    <p className="md:hidden block">Leave Data</p>                                                                        
+                                </th>
+                                <td scope="col" class=" hidden md:table-cell">
+                                    Start Time
+                                </td>
+                                <td scope="col" class=" hidden md:table-cell">
+                                    Start Date
+                                </td>
+                                <td scope="col" class=" hidden md:table-cell">
+                                    End Date
+                                </td>
+                                <td scope="col" class=" hidden md:table-cell">
+                                    Duration
+                                </td>
+                                <td scope="col" class=" hidden md:table-cell">
+                                    Status
+                                </td>
+                                <td scope="col" class="table-cell">
+                                    Action
+                                </td>
+                            </thead>
+                            <tbody>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div>
+
         )
     }
     else{
