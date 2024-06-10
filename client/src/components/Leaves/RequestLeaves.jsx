11578eebf76bf7ed3d2da4be3ac488f7105558e0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-10 13:39:03 +0530

    Update RequestLeaves.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Leaves/RequestLeaves.jsx
+++ client/src/components/Leaves/RequestLeaves.jsx
@@ -18,6 +18,35 @@ const RequestLeaves = () => {
         return (
             <div className='mx-4 my-8'>
                 <h1 className="text-gray-500 text-xl font-semibold">Requested Leaves</h1>
+                <div className="my-4">
+                    <div class="py-2 px-4 rounded shadow-md overflow-x-auto my-8 bg-white">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <th scope="col" class=" hidden md:table-cell">
+                                    Email
+                                </th>
+                                <td scope="col" class=" hidden md:table-cell">
+                                    Start Time
+                                </td>
+                                <td scope="col" class=" hidden md:table-cell">
+                                    Start Date
+                                </td>
+                                <td scope="col" class=" hidden md:table-cell">
+                                    End Date
+                                </td>
+                                <td scope="col" class=" hidden md:table-cell">
+                                    Dutarion
+                                </td>
+                                <td scope="col" class=" hidden md:table-cell">
+                                    Status
+                                </td>
+                                <td scope="col" class=" hidden md:table-cell">
+                                    Action
+                                </td>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
             </div>
         )
     }
