c7f7ae0fbe9067c2b04a71603c76ca747ea6b751
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-10 16:36:43 +0530

    Update Employee.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Employees/Employee.jsx
+++ client/src/components/Employees/Employee.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from 'react-countup'
+import axios from 'axios'
 
 const Employee = () => {
-  return (
-    <div>Employee</div>
-  )
+    const navigate = useNavigate()
+    //curent login user
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("Login2");
+
+    if(RoleUser === "SuperAdmin" || RoleUser === "Director" || RoleUser === "Secretary" || RoleUser === "HOD"){
+        return (
+            <div>Employee</div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
 }
 
 export default Employee
