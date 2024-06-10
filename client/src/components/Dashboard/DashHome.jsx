ed492956e3871254aa608a4064dbf70b59268b22
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-10 11:18:51 +0530

    Update DashHome.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Dashboard/DashHome.jsx
+++ client/src/components/Dashboard/DashHome.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import  secureLocalStorage  from  "react-secure-storage"
-import { BsBackpack2Fill, BsSpeedometer2, BsPersonVideo3, BsFileEarmarkText, BsPeople, BsPersonGear, BsPower, BsMortarboard, BsBuilding, BsMortarboardFill, BsPersonCheck, BsCashCoin, BsCalendarEvent, BsCashStack, BsCarFrontFill, BsPassFill, BsHouseGearFill, BsFileTextFill, BsTicketDetailedFill, BsBoxArrowDownRight, BsPeopleFill } from "react-icons/bs";
+import { BsBackpack2Fill, BsSpeedometer2, BsPersonVideo3, BsFileEarmarkText, BsPeople, BsPersonGear, BsPower, BsMortarboard, BsBuilding, BsMortarboardFill, BsPersonCheck, BsCashCoin, BsCalendarEvent, BsCashStack, BsCarFrontFill, BsPassFill, BsHouseGearFill, BsFileTextFill, BsTicketDetailedFill, BsBoxArrowDownRight, BsPeopleFill, BsBuildingFill } from "react-icons/bs";
 import CountUp from 'react-countup'
 
 const DashHome = () => {
@@ -41,6 +41,30 @@ const DashHome = () => {
 
             <div className="my-4">
               <div className="">
+                <div className="my-8 ml-4 mr-6">
+                {
+                  (() => {
+                    if(RoleUser === "HOD"){
+                      return (
+                        <div className={`bg-white w-full mx-2 md:my-0 my-2 duration-500 rounded shadow-md`}>
+                        <div className="flex py-6 px-8 w-full justify-between border border-gray-200 rounded">
+                            <div className="">
+                                <h1 className={`text-[180%] text-blue-500 font-semilbold`}>#</h1>
+                                <p className="py-2 text-[120%]">Computer Science</p>
+                            </div>
+                            <div className="">
+                                <p className="text-[200%] text-gray-500"><BsBuildingFill /></p>
+                            </div>
+                        </div>
+                        <div className="text-white py-4 px-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded">
+                            My Department
+                        </div>
+                      </div>
+                      )
+                    }
+                  })()
+                }
+                </div>
                 {
                   (() => {
                     if(RoleUser === "SuperAdmin" || RoleUser === "Director" || RoleUser === "Secretary" || RoleUser === "HOD"){
