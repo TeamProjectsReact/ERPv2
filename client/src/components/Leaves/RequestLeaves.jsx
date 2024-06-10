39d6db35e6eff72ea64cd4c80016abc1b85aa609
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-10 11:12:36 +0530

    Update RequestLeaves.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Leaves/RequestLeaves.jsx
+++ client/src/components/Leaves/RequestLeaves.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
+import axios from 'axios'
 
 const RequestLeaves = () => {
-  return (
-    <div className='mx-4 my-8'>
-        <h1 className="text-gray-500 text-xl font-semibold">Requested Leaves</h1>
-    </div>
-  )
+    const navigate = useNavigate()
+    //curent login user
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("Login2");
+
+    const [buttonValue, SetButtonValue] = useState(0)
+    const HeadleButtonClick = (clickValue) => {
+        SetButtonValue(clickValue)   
+    }
+
+    if(RoleUser === "SuperAdmin" || RoleUser === "Director" || RoleUser === "Secretary" || RoleUser === "HOD"){
+        return (
+            <div className='mx-4 my-8'>
+                <h1 className="text-gray-500 text-xl font-semibold">Requested Leaves</h1>
+            </div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
+
 }
 
 export default RequestLeaves
