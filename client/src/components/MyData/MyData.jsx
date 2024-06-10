e22951e36ce292cc1c1787fff5e874660a2a5c83
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-10 11:25:07 +0530

    Update MyData.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/MyData/MyData.jsx
+++ client/src/components/MyData/MyData.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from 'react-countup'
+import axios from 'axios'
 
 const MyData = () => {
-  return (
-    <div className='mx-8 my-4'>
-        <h1 className="text-gray-500 font-semibold text-xl">My Profile</h1>
-    </div>
-  )
+    const navigate = useNavigate()
+    //curent login user
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("Login2");
+
+    if(EmailUser !== null && RoleUser !== null){
+        return (
+            <div className='mx-8 my-4'>
+                <h1 className="text-gray-500 font-semibold text-xl">My Profile</h1>
+            </div>
+        )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        })
+    }
 }
 
 export default MyData
