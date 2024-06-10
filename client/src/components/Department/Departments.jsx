5836939b4104b864cb22f0755198893bc1df66ae
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-10 20:45:38 +0530

    Update Departments.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Department/Departments.jsx
+++ client/src/components/Department/Departments.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from 'react-countup'
+import axios from 'axios'
+
 
 const Departments = () => {
-  return (
-    <div>Departments</div>
-  )
+    const navigate = useNavigate()
+    //curent login user
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("Login2");
+
+    if(RoleUser === "SuperAdmin" || RoleUser === "Director" || RoleUser === "Secretary" ){
+        return (
+            <div className='mx-4'>
+                <div className="">
+                    <h1 className="text-gray-500 text-xl font-semibold">Departments</h1>
+                    <div className="">
+                        <Link>
+                            <button className='bg-green-600 text-white py-2 px-4 rounded diuration-500 hover:bg-green-700'>Add Department</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+          )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
 }
 
 export default Departments
