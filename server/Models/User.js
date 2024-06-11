305ad4796f38ee16fce749e491a9903b966c58a2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-11 07:33:57 +0530

    Update User.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Models/User.js
+++ server/Models/User.js
@@ -19,6 +19,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    Department: {
+        type: String,
+    },
     createdAt: {
         type: Date,
         default: Date.now
