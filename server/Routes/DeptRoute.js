e2ad70c556dde0ab48c472466f405672933f1663
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-11 07:27:04 +0530

    Update DeptRoute.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Routes/DeptRoute.js
+++ server/Routes/DeptRoute.js
@@ -4,6 +4,6 @@ const DeptController = require('../Controllers/DeptController')
 const router = express.Router()
 
 router.get('/UsertoDept', DeptController.SeeUsers)
-
+router.post('/AddDept', DeptController.AddDept)
 
 module.exports = router;
