14548462c681f42252772aa6c323ce6b692fd266
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-11 07:08:36 +0530

    Update AddDept.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Department/AddDept.jsx
+++ client/src/components/Department/AddDept.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage"
+import CountUp from 'react-countup'
+import axios from 'axios'
 
 const AddDept = () => {
-  return (
-    <div>AddDept</div>
-  )
+    const navigate = useNavigate()
+    //curent login user
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("Login2");
+
+    if(RoleUser === "SuperAdmin" || RoleUser === "Director" || RoleUser === "Secretary" ){
+        return (
+            <div>AddDept</div>
+          )
+    }
+    else{
+        useEffect(() => {
+            localStorage.clear()
+            navigate('/')
+        }, [])
+    }
 }
 
 export default AddDept
