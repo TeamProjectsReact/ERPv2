e7e2449799d70af616ccde9705979cb1e3ddd417
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-15 07:34:26 +0530

    Update Email.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Models/Email.js
+++ server/Models/Email.js
@@ -1,7 +1,22 @@
 const mongoose = require('mongoose')
 
 const EmailSchema = new mongoose.Schema({
-
+    to: {
+        type: String,
+        required: true
+    },
+    subject: { 
+        type: String, 
+        required: true 
+    },
+    body: { 
+        type: String, 
+        required: true 
+    },
+    sendAt: {
+        type: Date,
+        default: Date.now
+    }
 })
 
 const Email = mongoose.model("Email", EmailSchema)
