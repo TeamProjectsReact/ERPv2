556f8b56c5b58575ad63eef24eaa5db08aaf5ee2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-20 11:05:36 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Reservation/Reservation.jsx
+++ client/src/components/Reservation/Reservation.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import AddReservation from './AddReservation'
 
 const Reservation = () => {
   return (
-    <div className='ml-4'>Reservation</div>
+    <div className='ml-4'>
+      <h1 className="text-xl text-gray-500 font-semibold">
+        Reservation
+      </h1>
+      <AddReservation />
+    </div>
+
   )
 }
 
