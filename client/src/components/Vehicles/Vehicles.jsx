fe2a43ac228d9fb60e8068a187d114769ae94fe2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-20 11:11:06 +0530

    Update Vehicles.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Vehicles/Vehicles.jsx
+++ client/src/components/Vehicles/Vehicles.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BsBoxArrowDownRight, BsPersonFillAdd, BsPersonFillLock } from 'react-icons/bs'
+import { useNavigate } from 'react-router-dom'
+import  secureLocalStorage  from  "react-secure-storage";
+import CountUp from 'react-countup';
 
 const Vehicles = () => {
+    const navigate = useNavigate()
+    const EmailUser = secureLocalStorage.getItem("Login1");
+    const RoleUser = secureLocalStorage.getItem("Login2");
+
+    if(RoleUser === "SuperAdmin")
   return (
     <div>Vehicles</div>
   )
