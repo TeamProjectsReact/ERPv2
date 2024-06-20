d1b447c33a55a0aa8ad980466cbf10716e1cb52f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-20 11:07:34 +0530

    Update AddReservation.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Reservation/AddReservation.jsx
+++ client/src/components/Reservation/AddReservation.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const AddReservation = () => {
   return (
-    <div className='bg-white py-2 px-4'>AddReservation</div>
+    <div className='bg-white py-4 px-4 my-2 rounded shadow-md'>
+        <h1 className="text-gray-500 font-semibold">AddReservation</h1>
+        <hr className="" />
+
+        
+    </div>
   )
 }
 
