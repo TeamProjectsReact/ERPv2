91dc11c5021a4b8014d3efdba484b3d476270911
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-24 12:19:11 +0530

    Update DashHome.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Dashboard/DashHome.jsx
+++ client/src/components/Dashboard/DashHome.jsx
@@ -25,16 +25,6 @@ const DashHome = () => {
       {id: 9, name: "Departments", icon: <BsBuildingFill />, value: <CountUp end={20} />, style: "text-yellow-500"},                         
     ]
 
-    const userData = [
-      {id: 1, name: 'Leaves', value: <CountUp end={20} />, icon: <BsBoxArrowDownRight />, style: "text-green-500"},
-      {id: 2, name: 'Reservation', value: <CountUp end={20} />, icon: <BsTicketDetailedFill />, style: "text-red-500"},
-      {id: 3, name: 'SRNs', value: <CountUp end={20} />, icon: <BsFileTextFill />, style: "text-blue-500"},
-      {id: 4, name: 'Work Requests', value: <CountUp end={20} />, icon: <BsHouseGearFill />, style: "text-yellow-500"},
-      {id: 5, name: 'GatePass', value: <CountUp end={20} />, icon: <BsPassFill />, style: "text-purple-500"},
-      {id: 6, name: 'Increment', value: <CountUp end={20} />, icon: <BsCashStack />, style: "text-blue-500"},
-      {id: 7, name: 'Vehicle', value: <CountUp end={20} />, icon: <BsCarFrontFill />, style: "text-green-500"},
-    ]
-
     const [UserData, SetUserData] = useState([])
     useEffect(() => {
         axios.get('http://localhost:5000/Dept/MyDept/' + EmailUser)
@@ -51,7 +41,7 @@ const DashHome = () => {
 
             <div className="my-4">
               <div className="">
-                <div className="my-8 ml-4 mr-6">
+              <div className="my-8 ml-4 mr-6">
                 {
                   (() => {
                     if(RoleUser === "HOD"){
@@ -75,62 +65,70 @@ const DashHome = () => {
                   })()
                 }
                 </div>
-                {
-                  (() => {
-                    if(RoleUser === "SuperAdmin" || RoleUser === "Director" || RoleUser === "Secretary" || RoleUser === "HOD"){
-                      return (
-                        <div className="md:grid grid-cols-4 gap-4 mr-4">
+                <div className="md:grid grid-cols-4 gap-4 mr-4">
                         {
                           DashData.map((data) => {
-                            return(
-                              <div className={`bg-white w-full mx-2 md:my-0 my-2 duration-500 rounded shadow-md ${data.style}`}>
-                                <div className="flex py-6 px-8 w-full justify-between border border-gray-200 rounded">
-                                    <div className="">
-                                        <h1 className={`text-[180%] ${data.style}`}>{data.value}</h1>
-                                        <p className="py-2 text-[120%]">{data.name}</p>
-                                    </div>
-                                    <div className="">
-                                        <p className="text-[200%] text-gray-500">{data.icon}</p>
-                                    </div>
+                            if(RoleUser === "SuperAdmin" || RoleUser === "Director" || RoleUser === "Secretary"){
+                              return(
+                                <div className={`bg-white w-full mx-2 md:my-0 my-2 duration-500 rounded shadow-md ${data.style}`}>
+                                  <div className="flex py-6 px-8 w-full justify-between border border-gray-200 rounded">
+                                      <div className="">
+                                          <h1 className={`text-[180%] ${data.style}`}>{data.value}</h1>
+                                          <p className="py-2 text-[120%]">{data.name}</p>
+                                      </div>
+                                      <div className="">
+                                          <p className="text-[200%] text-gray-500">{data.icon}</p>
+                                      </div>
+                                  </div>
+                                  <div className="text-white py-4 px-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded">
+                                      All {data.name} 
+                                  </div>
                                 </div>
-                                <div className="text-white py-4 px-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded">
-                                    All {data.name} 
-                                </div>
-                              </div>
-                            )
+                              )
+                            }
+                            if(RoleUser === "HOD"){
+                              if(data.id !== 9){
+                                return(
+                                  <div className={`bg-white w-full mx-2 md:my-0 my-2 duration-500 rounded shadow-md ${data.style}`}>
+                                    <div className="flex py-6 px-8 w-full justify-between border border-gray-200 rounded">
+                                        <div className="">
+                                            <h1 className={`text-[180%] ${data.style}`}>{data.value}</h1>
+                                            <p className="py-2 text-[120%]">{data.name}</p>
+                                        </div>
+                                        <div className="">
+                                            <p className="text-[200%] text-gray-500">{data.icon}</p>
+                                        </div>
+                                    </div>
+                                    <div className="text-white py-4 px-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded">
+                                        All {data.name} 
+                                    </div>
+                                  </div>
+                                )
+                              }
+                            }
+                            else{
+                              if(data.id !== 1 && data.id !== 9){
+                                return(
+                                  <div className={`bg-white w-full mx-2 md:my-0 my-2 duration-500 rounded shadow-md ${data.style}`}>
+                                    <div className="flex py-6 px-8 w-full justify-between border border-gray-200 rounded">
+                                        <div className="">
+                                            <h1 className={`text-[180%] ${data.style}`}>{data.value}</h1>
+                                            <p className="py-2 text-[120%]">{data.name}</p>
+                                        </div>
+                                        <div className="">
+                                            <p className="text-[200%] text-gray-500">{data.icon}</p>
+                                        </div>
+                                    </div>
+                                    <div className="text-white py-4 px-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded">
+                                        All {data.name} 
+                                    </div>
+                                  </div>
+                                )
+                              }
+                            }
                           })
                         }                
-                      </div>
-                      )
-                    }
-                    if(RoleUser === "RA"){
-                      return (
-                        <div className="md:grid grid-cols-4 gap-4 mr-4">
-                          {
-                            userData.map((user) => {
-                              return (
-                                <div className={`bg-white w-full mx-2 md:my-0 my-2 duration-500 rounded shadow-md ${user.style}`}>
-                                <div className="flex py-6 px-8 w-full justify-between border border-gray-200 rounded">
-                                    <div className="">
-                                        <h1 className={`text-[180%] ${user.style}`}>{user.value}</h1>
-                                        <p className="py-2 text-[120%]">{user.name}</p>
-                                    </div>
-                                    <div className="">
-                                        <p className="text-[200%] text-gray-500">{user.icon}</p>
-                                    </div>
-                                </div>
-                                <div className="text-white py-4 px-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded">
-                                    My {user.name} 
-                                </div>
-                              </div>
-                              )
-                            })
-                          }
-                        </div>
-                      )
-                    }
-                  })()
-                } 
+                </div>
               </div>
             </div>
           </div>
