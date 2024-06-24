9bc56d265e4edea497ba55e2eb9fd14921414a58
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-24 14:13:21 +0530

    Update Leaves.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Leaves/Leaves.jsx
+++ client/src/components/Leaves/Leaves.jsx
@@ -7,10 +7,10 @@ import AddLeaves from './AddLeaves';
 
 const Leaves = () => {
   const LeaveData = [
-    {id: 1, btnValue: 0, name: "Leaves", icon: <BsBoxArrowDownRight />, value: <CountUp end={20} />, style: "text-blue-500"},
-    {id: 2, btnValue: "RequestLeaves", name: "Accepted", icon: <BsBoxArrowDownRight />, value: <CountUp end={20} />, style: "text-yellow-500"},
-    {id: 3, btnValue: "RecommendLeaves", name: "Approved", icon: <BsBoxArrowDownRight />, value: <CountUp end={20} />, style: "text-green-500"},   
-    {id: 3, btnValue: "RejectLeaves", name: "Reject", icon: <BsBoxArrowDownRight />, value: <CountUp end={20} />, style: "text-red-500"},     
+    {id: 1, bgstyle: 'bg-blue-400/20', borderStyle: 'border-blue-500', btnValue: 0, name: "Leaves", icon: <BsBoxArrowDownRight />, value: <CountUp end={20} />, style: "text-blue-500"},
+    {id: 2, bgstyle: 'bg-yellow-400/20', borderStyle: 'border-yellow-500', btnValue: "RequestLeaves", name: "Accepted", icon: <BsBoxArrowDownRight />, value: <CountUp end={20} />, style: "text-yellow-500"},
+    {id: 3, bgstyle: 'bg-green-400/20', borderStyle: 'border-green-500', btnValue: "RecommendLeaves", name: "Approved", icon: <BsBoxArrowDownRight />, value: <CountUp end={20} />, style: "text-green-500"},   
+    {id: 3, bgstyle: 'bg-red-400/20', borderStyle: 'border-red-500', btnValue: "RejectLeaves", name: "Reject", icon: <BsBoxArrowDownRight />, value: <CountUp end={20} />, style: "text-red-500"},     
   ]
 
   const navigate = useNavigate()
@@ -38,18 +38,15 @@ const Leaves = () => {
                             {
                               LeaveData.map((data) => {
                                 return(
-                                  <div onClick={() => HeadleButtonClick(data.btnValue)} className={`cursor-pointer bg-white w-full mx-2 md:my-0 my-2 duration-500 rounded shadow-md ${data.style}`}>
-                                    <div className="flex py-6 px-8 w-full justify-between border border-gray-200 rounded">
+                                  <div className={`md:my-0 my-2 ${data.bgstyle} text-white px-4 py-8 rounded border-l-4 shadow-lg shadow-red ${data.borderStyle}`}>
+                                    <div className="flex justify-between">
                                         <div className="">
-                                            <h1 className={`text-[180%] ${data.style}`}>{data.value}</h1>
-                                            <p className="py-2 text-[120%]">{data.name}</p>
+                                            <h1 className={`text-3xl font-semibold ${data.style}`}>{data.value}</h1>
+                                            <p className={`${data.style}`}>{data.name}</p> 
                                         </div>
                                         <div className="">
-                                            <p className="text-[200%] text-gray-500">{data.icon}</p>
+                                            <p className={`text-4xl ${data.style} pt-2`}>{data.icon}</p>
                                         </div>
-                                    </div>
-                                    <div className="text-white py-4 px-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded">
-                                        All {data.name} 
                                     </div>
                                   </div>
                                 )
