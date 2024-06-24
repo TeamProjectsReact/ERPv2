d726ba262203f327521c4ee1d56efae540212823
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-24 12:02:38 +0530

    Update DashSide.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Dashboard/DashSide.jsx
+++ client/src/components/Dashboard/DashSide.jsx
@@ -20,16 +20,16 @@ const DashSide = () => {
     };
 
     const SideLink = [
-        {id: 1, name: "Dashboard", link: "Home", icons: <BsSpeedometer2 />},    
-        {id: 2, name: "Leaves", link: "Leaves", icons: <BsBoxArrowDownRight />},  
-        {id: 3, name: "Reservation", link: "Reservation", icons: <BsTicketDetailedFill />},  
-        {id: 4, name: "SRNs", link: "SRNs", icons: <BsFileTextFill />},  
-        {id: 5, name: "Work Requests", link: "WorkRequests", icons: <BsHouseGearFill />},  
-        {id: 7, name: "GatePass", link: "GatePass", icons: <BsPassFill />},  
-        {id: 8, name: "Increment", link: "Increment", icons: <BsCashStack />},  
-        {id: 9, name: "Vehicle", link: "Vehicle", icons: <BsCarFrontFill />},  
-        {id: 10, name: "SiteAdmin", link: "SiteAdmins", icons: <BsPersonFillLock />},  
-        {id: 11, name: "Employee", link: "Employee", icons: <BsPeopleFill />},          
+        {style: 'text-orange-500', id: 1, name: "Dashboard", link: "Home", icons: <BsSpeedometer2 />},    
+        {style: 'text-yellow-500', id: 2, name: "Leaves", link: "Leaves", icons: <BsBoxArrowDownRight />},  
+        {style: 'text-green-500', id: 3, name: "Reservation", link: "Reservation", icons: <BsTicketDetailedFill />},  
+        {style: 'text-purple-500', id: 4, name: "SRNs", link: "SRNs", icons: <BsFileTextFill />},  
+        {style: 'text-pink-500', id: 5, name: "Work Requests", link: "WorkRequests", icons: <BsHouseGearFill />},  
+        {style: 'text-red-500', id: 7, name: "GatePass", link: "GatePass", icons: <BsPassFill />},  
+        {style: 'text-blue-500', id: 8, name: "Increment", link: "Increment", icons: <BsCashStack />},  
+        {style: 'text-green-500', id: 9, name: "Vehicle", link: "Vehicle", icons: <BsCarFrontFill />},  
+        {style: 'text-yellow-500', id: 10, name: "SiteAdmin", link: "SiteAdmins", icons: <BsPersonFillLock />},  
+        {style: 'text-purple-600', id: 11, name: "Employee", link: "Employee", icons: <BsPeopleFill />},          
     ]
 
     const [UserNameDropDown, SetUserNameDropDown] = useState(false);
