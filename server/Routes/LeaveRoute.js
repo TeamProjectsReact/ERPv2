c1769c3ce9f40745f59d769bfd77f23b89f816b9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-24 14:44:28 +0530

    Update LeaveRoute.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Routes/LeaveRoute.js
+++ server/Routes/LeaveRoute.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get('/hodData', LeaveController.HodData);
 router.post('/AddLeave/:id', LeaveController.AddLeave);
-router.get('/LeavesForHOD/:id', LeaveController.hodEmailLeaves);
+router.get('/AllLeaves', LeaveController.hodEmailLeaves);
 router.post('/AcceptLeave/:id', LeaveController.acceptLeave);
 router.post('/LeaveRollback/:id', LeaveController.LeaveRollback);
 router.post('/LeaveRejected/:id', LeaveController.RejectLeave);
