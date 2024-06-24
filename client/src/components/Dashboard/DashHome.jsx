71b4294f9cc8bda8ca4a843f0008aeb160b371cd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-06-24 12:56:36 +0530

    Update DashHome.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- client/src/components/Dashboard/DashHome.jsx
+++ client/src/components/Dashboard/DashHome.jsx
@@ -15,14 +15,14 @@ const DashHome = () => {
     // dashdata
     const DashData = [
       {id: 1, name: "Employees", bgstyle: 'bg-green-400/20', borderStyle: 'border-green-500', icon: <BsPeopleFill />, value: <CountUp end={20} />, style: "text-green-500"},
-      {id: 2, name: "Leaves", icon: <BsBoxArrowDownRight />, value: <CountUp end={20} />, style: "text-blue-500"},
-      {id: 3, name: "Reservation", icon: <BsTicketDetailedFill />, value: <CountUp end={20} />, style: "text-yellow-500"},
-      {id: 4, name: "SRNs", icon: <BsFileTextFill />, value: <CountUp end={20} />, style: "text-red-500"},
-      {id: 5, name: "Work Requests", icon: <BsHouseGearFill />, value: <CountUp end={20} />, style: "text-purple-500"},
-      {id: 6, name: "GatePass", icon: <BsPassFill />, value: <CountUp end={20} />, style: "text-blue-500"},
-      {id: 7, name: "Increment", icon: <BsCashStack />, value: <CountUp end={20} />, style: "text-green-500"},  
-      {id: 8, name: "Vehicle", icon: <BsCarFrontFill />, value: <CountUp end={20} />, style: "text-yellow-500"},   
-      {id: 9, name: "Departments", icon: <BsBuildingFill />, value: <CountUp end={20} />, style: "text-yellow-500"},                         
+      {id: 2, name: "Leaves", bgstyle: 'bg-blue-400/20', borderStyle: 'border-blue-500', icon: <BsBoxArrowDownRight />, value: <CountUp end={20} />, style: "text-blue-500"},
+      {id: 3, name: "Reservation", bgstyle: 'bg-yellow-400/20', borderStyle: 'border-yellow-500', icon: <BsTicketDetailedFill />, value: <CountUp end={20} />, style: "text-yellow-500"},
+      {id: 4, name: "SRNs", bgstyle: 'bg-red-400/20', borderStyle: 'border-red-500', icon: <BsFileTextFill />, value: <CountUp end={20} />, style: "text-red-500"},
+      {id: 5, name: "Work Requests", bgstyle: 'bg-purple-400/20', borderStyle: 'border-purple-500', icon: <BsHouseGearFill />, value: <CountUp end={20} />, style: "text-purple-500"},
+      {id: 6, name: "GatePass", bgstyle: 'bg-blue-400/20', borderStyle: 'border-blue-500', icon: <BsPassFill />, value: <CountUp end={20} />, style: "text-blue-500"},
+      {id: 7, name: "Increment", bgstyle: 'bg-green-400/20', borderStyle: 'border-green-500', icon: <BsCashStack />, value: <CountUp end={20} />, style: "text-green-500"},  
+      {id: 8, name: "Vehicle", bgstyle: 'bg-yellow-400/20', borderStyle: 'border-yellow-500', icon: <BsCarFrontFill />, value: <CountUp end={20} />, style: "text-yellow-500"},   
+      {id: 9, name: "Departments", bgstyle: 'bg-red-400/20', borderStyle: 'border-red-500', icon: <BsBuildingFill />, value: <CountUp end={20} />, style: "text-red-500"},                         
     ]
 
     const [UserData, SetUserData] = useState([])
@@ -70,20 +70,17 @@ const DashHome = () => {
                           DashData.map((data) => {
                             if(RoleUser === "SuperAdmin" || RoleUser === "Director" || RoleUser === "Secretary"){
                               return(
-                                <div className={`bg-white w-full mx-2 md:my-0 my-2 duration-500 rounded shadow-md ${data.style}`}>
-                                  <div className="flex py-6 px-8 w-full justify-between border border-gray-200 rounded">
+                                <div className={`md:my-0 my-2 ${data.bgstyle} text-white px-4 py-8 rounded border-l-4 shadow-lg shadow-red ${data.borderStyle}`}>
+                                  <div className="flex justify-between">
                                       <div className="">
-                                          <h1 className={`text-[180%] ${data.style}`}>{data.value}</h1>
-                                          <p className="py-2 text-[120%]">{data.name}</p>
+                                          <h1 className={`text-3xl font-semibold ${data.style}`}>{data.value}</h1>
+                                          <p className={`${data.style}`}>{data.name}</p> 
                                       </div>
                                       <div className="">
-                                          <p className="text-[200%] text-gray-500">{data.icon}</p>
+                                          <p className={`text-4xl ${data.style} pt-2`}>{data.icon}</p>
                                       </div>
                                   </div>
-                                  <div className="text-white py-4 px-8 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded">
-                                      All {data.name} 
-                                  </div>
-                                </div>
+                              </div>
                               )
                             }
                             if(RoleUser === "HOD"){
