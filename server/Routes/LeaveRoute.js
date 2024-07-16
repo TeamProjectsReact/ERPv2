65d9803d5bb0e8ef64c70d796e8bc915b3271412
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-16 15:06:55 +0530

    Update LeaveRoute.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Routes/LeaveRoute.js
+++ server/Routes/LeaveRoute.js
@@ -11,5 +11,6 @@ router.post('/LeaveRollback/:id', LeaveController.LeaveRollback);
 router.post('/LeaveRejected/:id', LeaveController.RejectLeave);
 router.delete('/LeaveDelete/:id', LeaveController.DeleteLeave);
 router.get('/getUserLeave/:id', LeaveController.CountLeavs);
+router.get('/CountreqLeavs')
 
 module.exports = router;
