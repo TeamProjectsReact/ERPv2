ad05c5df4c5bed53bb1c8a73bef5108df291e2a0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-16 15:21:28 +0530

    Update UserRoute.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.post('/SignUp', authController.SignUp)
 router.post('/SignIn', authController.SigIn)
+router.post('/UpdateCurrentPass')
 
 module.exports = router;
