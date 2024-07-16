09a9fce9f2f61eb757c32e4e08b56c305c8147a6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-16 15:02:21 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>
    Co-Authored-By: Anupa Gamage <125038143+Anupa1998@users.noreply.github.com>

--- server/Controllers/EmployeeController.js
+++ server/Controllers/EmployeeController.js
@@ -1,7 +1,21 @@
 const User = require('../Models/User')
 
 const EmployeeController = {
-    
+    CountEmployee: async (req, res) =>{
+        try{
+            const countEmp = await User.countDocuments()
+
+            if(countEmp){
+                return res.json({ Result: countEmp })
+            }
+            else{
+                return res.json({ Error: "Internal Server Error"})
+            }
+        }
+        catch (err) {
+            console.log(err)
+        }
+    }
 }
 
 module.exports = EmployeeController
